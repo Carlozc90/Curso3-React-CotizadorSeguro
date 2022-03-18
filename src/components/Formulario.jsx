fe49d1de8137bdc6dc78902967a16a215774dbc5
Carlos Cancines <Carlozc90@gmail.com>
2022-03-18 00:04:57 -0500

    inicio del cotizador

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,88 @@
+import { Fragment } from "react";
+import { MARCAS, YEARS, PLANES } from "../constants";
+import useCotizador from "../hooks/useCotizador";
+import Error from "./Error";
+
+const Formulario = () => {
+  const { handleChangeDatos, datos, setError, error } = useCotizador();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (Object.values(datos).includes("")) {
+      setError("error, Campos obligatorios");
+      return;
+    }
+
+    setError("");
+  };
+  return (
+    <>
+      {error && <Error />}
+      <form onSubmit={handleSubmit}>
+        <div className="my-5">
+          <label className="block mb-3 font-bold text-gray-400 uppercase">
+            Marca
+          </label>
+          <select
+            name="marca"
+            className="w-full p-3 bg-white border border-gray-200"
+            onChange={(e) => handleChangeDatos(e)}
+            value={datos.marca}
+          >
+            <option value="">-- Seleccion Marca --</option>
+            {MARCAS.map((item) => (
+              <option key={item.id} value={item.id}>
+                {item.nombre}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="my-5">
+          <label className="block mb-3 font-bold text-gray-400 uppercase">
+            Año
+          </label>
+          <select
+            name="year"
+            className="w-full p-3 bg-white border border-gray-200"
+            onChange={(e) => handleChangeDatos(e)}
+            value={datos.year}
+          >
+            <option value="">-- Seleccion Año --</option>
+            {YEARS.map((item) => (
+              <option key={item} value={item}>
+                {item}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="my-5">
+          <label className="block mb-3 font-bold text-gray-400 uppercase">
+            Elige un Plan
+          </label>
+          <div className="flex gap-3 items-center">
+            {PLANES.map((item) => (
+              <Fragment key={item.id}>
+                <label>{item.nombre}</label>
+                <input
+                  type="radio"
+                  name="plan"
+                  value={item.id}
+                  onChange={(e) => handleChangeDatos(e)}
+                />
+              </Fragment>
+            ))}
+          </div>
+        </div>
+
+        <input
+          type="submit"
+          className="w-full bg-indigo-500 hover:bg-indigo-600 transition-colors text-white cursor-pointer p-3 uppercase font-bold"
+          value="Cotizar"
+        />
+      </form>
+    </>
+  );
+};
+
+export default Formulario;
---
+++ src/context/CotizadorProvider.jsx
@@ -0,0 +1,32 @@
+import { createContext, useState } from "react";
+
+const CotizadorContext = createContext();
+
+const CotizadorProvider = ({ children }) => {
+  const [datos, setDatos] = useState({
+    marca: "",
+    year: "",
+    plan: "",
+  });
+
+  const [error, setError] = useState("");
+
+  const handleChangeDatos = (e) => {
+    setDatos({
+      ...datos,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <CotizadorContext.Provider
+      value={{ handleChangeDatos, datos, setError, error }}
+    >
+      {children}
+    </CotizadorContext.Provider>
+  );
+};
+
+export { CotizadorProvider };
+
+export default CotizadorContext;
